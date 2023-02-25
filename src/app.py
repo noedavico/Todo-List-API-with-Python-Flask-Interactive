75fd052cfa5b760bac842fb9949aabfba6f2cea2
noedavico <noedavico@hotmail.com>
2023-02-25 18:21:27 +0000

    entrega

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 app = Flask(__name__)
 from flask import request
 
@@ -13,13 +13,13 @@ def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
     todos.append(request_body)
-    return jsonify(todos)  
+    return jsonify(todos) 
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    todos.pop(2)
+    todos.pop(position)
     print("This is the position to delete: ",position)
-    return 'something'
+    return  jsonify(todos)   
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
